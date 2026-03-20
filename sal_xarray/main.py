29eb03013af2da435e29279094453236d916145b
Samuel Jackson <samuel.jackson@ukaea.uk>
2026-03-20 10:44:14 +0000

    Fix linter warning

--- sal_xarray/main.py
+++ sal_xarray/main.py
@@ -27,7 +27,7 @@ class SALBackendEntrypoint(BackendEntrypoint):
         *,
         drop_variables: str | Iterable[str] | None = None,
         host: str = "https://sal.jetdata.eu",
-        **kwargs,
+        **kwargs,  # pylint: disable=unused-argument
     ) -> xr.Dataset:
         """Open a SAL dataset given a signal name and shot number.
 
@@ -100,7 +100,7 @@ class SALBackendEntrypoint(BackendEntrypoint):
 
         return data
 
-    def open_datatree(self, filename_or_obj, *, drop_variables=None, **kwargs):
+    def open_datatree(self, filename_or_obj, *, drop_variables=None, **kwargs):  # pylint: disable=unused-argument
         dataset = self.open_dataset(filename_or_obj, drop_variables=drop_variables)
         return xr.DataTree(dataset)
 
